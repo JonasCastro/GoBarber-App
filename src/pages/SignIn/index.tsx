131b4d798b905eca51323556f69c4b2f2a5ebf19
Jonas Castro <jonascastro128@gmail.com>
2020-07-12 18:15:21 -0300

    Authentication screen

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,24 +1,56 @@
 import React from 'react';
-import { Image } from 'react-native';
-
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 import logoImg from '../../assets/logo.png';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title> Faça seu logon </Title>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      enabled
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <ScrollView
+        contentContainerStyle={{ flex: 1 }}
+        keyboardShouldPersistTaps="handled"
+      >
+        <Container>
+          <Image source={logoImg} />
+          <View>
+            <Title> Faça seu logon </Title>
+          </View>
+          <Input name="email" icon="mail" placeholder="E-mail" />
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
-
-      <Input name="password" icon="lock" placeholder="Senha" />
-      <Button>Entrar</Button>
-    </Container>
+          <Input name="password" icon="lock" placeholder="Senha" />
+          <Button>Entrar</Button>
+          <ForgotPassword>
+            <ForgotPasswordText>Esqueci minha Senha</ForgotPasswordText>
+          </ForgotPassword>
+        </Container>
+      </ScrollView>
+      <CreateAccountButton>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </KeyboardAvoidingView>
   );
 };
 
