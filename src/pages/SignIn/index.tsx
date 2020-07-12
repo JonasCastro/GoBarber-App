bb4b5a6c9f461825a34767b70dcc3270d676c53a
Jonas Castro <jonascastro128@gmail.com>
2020-07-12 18:40:09 -0300

    Registration screen

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -7,6 +7,7 @@ import {
   Platform,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 import logoImg from '../../assets/logo.png';
 
 import Input from '../../components/Input';
@@ -22,6 +23,7 @@ import {
 } from './styles';
 
 const SignIn: React.FC = () => {
+  const navigation = useNavigation();
   return (
     <KeyboardAvoidingView
       style={{ flex: 1 }}
@@ -46,7 +48,7 @@ const SignIn: React.FC = () => {
           </ForgotPassword>
         </Container>
       </ScrollView>
-      <CreateAccountButton>
+      <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
         <Icon name="log-in" size={20} color="#ff9000" />
         <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
       </CreateAccountButton>
